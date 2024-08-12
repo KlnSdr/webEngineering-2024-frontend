0f14739b1c805ee5e5891d76c402e28f23cb00d9
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-08-12 16:18:20 +0200

    [FRONTEND-7]: increase default textarea height

--- frontend-react/src/components/TextArea.tsx
+++ frontend-react/src/components/TextArea.tsx
@@ -17,6 +17,7 @@ const TextArea: React.FC<TextAreaProps> = ({ initialValue, onChange }) => {
         onChange(e.target.value);
       }}
       className="form-control"
+    rows={10}
     ></textarea>
   );
 };

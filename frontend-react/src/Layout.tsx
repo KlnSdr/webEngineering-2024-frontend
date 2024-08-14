99eacda5799f22610b3fe21a383cf385f92800f3
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-08-14 14:15:37 +0200

    [FRONTEND-11]: style navbar using bootstraps navbar

--- frontend-react/src/Layout.tsx
+++ frontend-react/src/Layout.tsx
@@ -6,24 +6,47 @@ import "./style/App.css";
 function Layout() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/recipe">neues Rezept</Link>
-          </li>
-          <li>
-            <Link to="/search">Suche</Link>
-          </li>
-          <li>
-            <Link to="/survey">Umfrage</Link>
-          </li>
-          <li>
-            <Link to="/logout">Abmelden</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/recipe">
+                neues Rezept
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/search">
+                Suche
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/survey">
+                Umfrage
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/logout">
+                Abmelden
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       <div className="App">

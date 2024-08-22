ebc2d315bbaf28a356fb6ebfb3fbd19eda2446b9
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-08-22 22:04:34 +0200

    [FRONTEND-8]: Initialize product fetching logic in SearchView

--- frontend-react/src/views/SearchView.tsx
+++ frontend-react/src/views/SearchView.tsx
@@ -1,5 +1,26 @@
+import React, { useEffect, useState } from "react";
+import AddProductLine from "../components/AddProductLine";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import Stack from "react-bootstrap/Stack";
+import Heading, { Heading2 } from "../components/Heading";
+import ImageArea from "../components/ImageArea";
+import { NeededProduct, Product } from "../types/Products";
+import {ProductsService} from "../services/ProductService";
+import {RecipeService} from "../services/RecipeService";
+
 function SearchView() {
-  return <div>Hello Search</div>;
+  const [neededProducts, setNeededProducts] = useState<NeededProduct[]>([]);
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [productsData, setProductsData] = useState<Product[]>([]);
+
+    //Fetch products from the backend
+    useEffect(() => {
+        ProductsService.getAll().then((products) => {
+                setProductsData(products);
+            });
+    }, []);
+
 }
 
 export default SearchView;

e0e4c57ab134fe207cda8b4daf75ed4ba07189d7
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-08-30 00:04:53 +0200

    [FRONTEND-8]: using Promise<Recipe[]> as the return type for the searchRecipesByProducts function

--- frontend-react/src/services/RecipeService.ts
+++ frontend-react/src/services/RecipeService.ts
@@ -93,49 +93,69 @@ class RecipeService {
     });
   }
 
-    public static searchRecipesByProducts(neededProducts: NeededProduct[]): Promise<CreateRecipe[]> {
+    public static searchRecipesByProducts(neededProducts: NeededProduct[]): Promise<Recipe[]> {
         return new Promise((resolve, reject) => {
             this.getAll1()
                 .then((recipes) => {
 
-                    const staticRecipes: CreateRecipe[] = [
+                    const staticRecipes: Recipe[] = [
                         {
+                            id: 1,
                             title: "Käsesosse 1",
-                            image: null,
                             description: "A delicious cheese sauce.",
+                            image: "https://elavegan.com/de/wp-content/uploads/sites/5/2018/03/vegane-K%C3%A4sesauce-einfaches-Rezept-paleo-glutenfrei.jpg",
+                            isPrivate: false,
+                            creationDate: new Date(),
+                            ownerUri: "",
+                            likedByUserUris: [],
                             products: [
-                                { id: 7, productName: "Cheese", amount: 1 },
-                                { id: 4, productName: "Milk", amount: 1 },
-                                { id: 6, productName: "Butter", amount: 1 },
+                                { name: "Cheese", amount: 1, unit: "kg" },
+                                { name: "Milk", amount: 1, unit: "l" },
+                                { name: "Butter", amount: 0.5, unit: "kg" }, // Adjusted unit to kg
                             ],
                         },
                         {
+                            id: 2,
                             title: "Käsesosse 2",
-                            image: null,
                             description: "Cheese sauce with salt.",
+                            image: "https://elavegan.com/de/wp-content/uploads/sites/5/2018/03/vegane-K%C3%A4sesauce-einfaches-Rezept-paleo-glutenfrei.jpg",
+                            isPrivate: false,
+                            creationDate: new Date(),
+                            ownerUri: "",
+                            likedByUserUris: [],
                             products: [
-                                { id: 7, productName: "Cheese", amount: 2 },
-                                { id: 10, productName: "Salt", amount: 1 },
-                                { id: 4, productName: "Milk", amount: 2 },
+                                { name: "Cheese", amount: 2, unit: "kg" },
+                                { name: "Salt", amount: 0.1, unit: "kg" }, // Adjusted unit to kg
+                                { name: "Milk", amount: 2, unit: "l" },
                             ],
                         },
                         {
+                            id: 3,
                             title: "Käsesosse 3",
-                            image: null,
                             description: "A spicy cheese sauce.",
+                            image: "https://elavegan.com/de/wp-content/uploads/sites/5/2018/03/vegane-K%C3%A4sesauce-einfaches-Rezept-paleo-glutenfrei.jpg",
+                            isPrivate: false,
+                            creationDate: new Date(),
+                            ownerUri: "",
+                            likedByUserUris: [],
                             products: [
-                                { id: 7, productName: "Cheese", amount: 2 },
-                                { id: 11, productName: "Pepper", amount: 1 },
-                                { id: 2, productName: "Milk", amount: 1 },
+                                { name: "Cheese", amount: 2, unit: "kg" },
+                                { name: "Pepper", amount: 0.05, unit: "kg" }, // Adjusted unit to kg
+                                { name: "Milk", amount: 1, unit: "l" },
                             ],
                         },
                         {
+                            id: 4,
                             title: "Käsesosse 4",
-                            image: null,
                             description: "A mild cheese sauce.",
+                            image: "https://elavegan.com/de/wp-content/uploads/sites/5/2018/03/vegane-K%C3%A4sesauce-einfaches-Rezept-paleo-glutenfrei.jpg",
+                            isPrivate: false,
+                            creationDate: new Date(),
+                            ownerUri: "",
+                            likedByUserUris: [],
                             products: [
-                                { id: 7, productName: "Cheese", amount: 3 },
-                                { id: 2, productName: "Milk", amount: 2 }
+                                { name: "Cheese", amount: 3, unit: "kg" },
+                                { name: "Milk", amount: 2, unit: "l" },
                             ],
                         },
                     ];

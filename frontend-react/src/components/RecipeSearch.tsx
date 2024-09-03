7f546df35b1ee834259449481581caa9334cca83
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-03 18:46:07 +0200

    [FRONTEND-18]: add title input to create survey view

---
+++ frontend-react/src/components/RecipeSearch.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface RecipeSearchProps {
+}
+
+const RecipeSearch: React.FC<RecipeSearchProps> = ({}) => {
+    return (
+        <div>
+            RecipeSearch
+        </div>
+    );
+}
+
+export default RecipeSearch;
--- frontend-react/src/views/CreateSurveyView.tsx
+++ frontend-react/src/views/CreateSurveyView.tsx
@@ -1,22 +1,51 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import "../style/CreateRecipeView.css";
 import {UserService} from "../services/UserService";
 import {useNavigate} from "react-router-dom";
+import LabelInput from "../components/LabelInput";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+import RecipeSearch from "../components/RecipeSearch";
 
 function CreateSurveyView() {
-  const navigate = useNavigate();
-  useEffect(() => {
-      UserService.isLoggedIn().then((isLoggedIn: boolean) => {
-          if (!isLoggedIn) {
-              navigate("/login");
-          }
-      });
-  }, [navigate]);
+    const emptySurvey = {
+        title: "", options: []
+    };
+    const [state, setState] = useState(emptySurvey);
 
-  return (
-    <div>
-    </div>
-  );
+    const navigate = useNavigate();
+    useEffect(() => {
+        UserService.isLoggedIn().then((isLoggedIn: boolean) => {
+            if (!isLoggedIn) {
+                navigate("/login");
+            }
+        });
+    }, [navigate]);
+
+    return (<div>
+        <LabelInput
+            labelText="Title"
+            initialValue={state.title}
+            onChange={(val: string) => {
+                setState({...state, title: val});
+            }}
+        />
+        <RecipeSearch/>
+        <Stack direction="horizontal">
+            <Button
+                variant="secondary"
+                onClick={() => {
+                }}
+                className="ms-auto"
+            >
+                verwerfen
+            </Button>
+            <Button variant="primary" onClick={() => {
+            }}>
+                speichern
+            </Button>
+        </Stack>
+    </div>);
 }
 
 export default CreateSurveyView;

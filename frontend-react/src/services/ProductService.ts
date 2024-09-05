2686de115249fc75819ab735f94b5512f8534bf0
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-09-05 12:07:02 +0200

    [FRONTEND-13]: add getProductIdByName function for the fridgeService

--- frontend-react/src/services/ProductService.ts
+++ frontend-react/src/services/ProductService.ts
@@ -1,4 +1,4 @@
-import { Product } from "../types/Products";
+import {NeededProduct, Product} from "../types/Products";
 
 class ProductsService {
   private static backendURL: string =
@@ -19,6 +19,15 @@ class ProductsService {
         });
     });
   }
+
+  //get Id from product
+    public static getProductIdByName(productName: string): Promise<NeededProduct | null> {
+        return this.getAll().then(products => {
+            const product = products.find(prod => prod.name.toLowerCase() === productName.toLowerCase());
+            return product ? { id: product.id, productName: product.name, amount: 0 } : null;
+        });
+    }
+
 }
 
 export { ProductsService };

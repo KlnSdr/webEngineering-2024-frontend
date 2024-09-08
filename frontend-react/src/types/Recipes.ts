5d153c978bc0efb6ab4b8485c285de278a76d0d5
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-09 00:10:57 +0200

    [ORGA-6]: type docs

--- frontend-react/src/types/Recipes.ts
+++ frontend-react/src/types/Recipes.ts
@@ -1,23 +1,97 @@
-import {DetailedProduct, NeededProduct, Product} from "./Products";
+import { DetailedProduct, NeededProduct, Product } from "./Products";
 
+/**
+ * Interface representing the structure of a recipe to be created.
+ */
 interface CreateRecipe {
+  /**
+   * The title of the recipe.
+   * @type {string}
+   */
   title: string;
+
+  /**
+   * The image URL of the recipe.
+   * @type {string | null}
+   */
   image: string | null;
+
+  /**
+   * The description of the recipe.
+   * @type {string}
+   */
   description: string;
+
+  /**
+   * Indicates if the recipe is private.
+   * @type {boolean}
+   */
   isPrivate: boolean;
+
+  /**
+   * The list of needed products for the recipe.
+   * @type {NeededProduct[]}
+   */
   products: NeededProduct[];
 }
 
+/**
+ * Interface representing the structure of a recipe.
+ */
 interface Recipe {
-    id: number;
-    title: string;
-    description: string;
-    imgUri: string;
-    isPrivate: boolean;
-    creationDate: Date;
-    ownerUri: string;
-    likedByUserUris: string[];
-    products: DetailedProduct[];
+  /**
+   * The unique identifier of the recipe.
+   * @type {number}
+   */
+  id: number;
+
+  /**
+   * The title of the recipe.
+   * @type {string}
+   */
+  title: string;
+
+  /**
+   * The description of the recipe.
+   * @type {string}
+   */
+  description: string;
+
+  /**
+   * The image URL of the recipe.
+   * @type {string}
+   */
+  imgUri: string;
+
+  /**
+   * Indicates if the recipe is private.
+   * @type {boolean}
+   */
+  isPrivate: boolean;
+
+  /**
+   * The creation date of the recipe.
+   * @type {Date}
+   */
+  creationDate: Date;
+
+  /**
+   * The URI of the owner of the recipe.
+   * @type {string}
+   */
+  ownerUri: string;
+
+  /**
+   * The list of URIs of users who liked the recipe.
+   * @type {string[]}
+   */
+  likedByUserUris: string[];
+
+  /**
+   * The list of detailed products for the recipe.
+   * @type {DetailedProduct[]}
+   */
+  products: DetailedProduct[];
 }
 
-export type { CreateRecipe,Recipe };
+export type { CreateRecipe, Recipe };
--- frontend-react/src/types/Surveys.ts
+++ frontend-react/src/types/Surveys.ts
@@ -1,29 +1,114 @@
-import {Recipe} from "./Recipes";
+import { Recipe } from "./Recipes";
 
-interface Survey{
+/**
+ * Interface representing a survey.
+ */
+interface Survey {
+    /**
+     * The unique identifier of the survey.
+     * @type {number}
+     */
     id: number;
+
+    /**
+     * The title of the survey.
+     * @type {string}
+     */
     title: string;
+
+    /**
+     * The list of participants' identifiers.
+     * @type {string[]}
+     */
     participants: string[];
+
+    /**
+     * The creator's identifier.
+     * @type {string}
+     */
     creator: string;
-    recipeVote: {[key: string]: number};
+
+    /**
+     * The votes for each recipe, keyed by recipe identifier.
+     * @type {{[key: string]: number}}
+     */
+    recipeVote: { [key: string]: number };
+
+    /**
+     * The list of option identifiers.
+     * @type {string[]}
+     */
     options: string[];
+
+    /**
+     * The creation date of the survey.
+     * @type {Date}
+     */
     creationDate: Date;
 }
 
+/**
+ * Interface representing an update to a survey.
+ */
 interface UpdateSurvey {
+    /**
+     * The unique identifier of the survey.
+     * @type {number}
+     */
     id: number;
+
+    /**
+     * The title of the survey.
+     * @type {string}
+     */
     title: string;
+
+    /**
+     * The list of participants' identifiers.
+     * @type {string[]}
+     */
     participants: string[];
+
+    /**
+     * The creator's identifier.
+     * @type {string}
+     */
     creator: string;
-    recipeVote: {[key: string]: number};
+
+    /**
+     * The votes for each recipe, keyed by recipe identifier.
+     * @type {{[key: string]: number}}
+     */
+    recipeVote: { [key: string]: number };
+
+    /**
+     * The list of recipe options.
+     * @type {Recipe[]}
+     */
     options: Recipe[];
+
+    /**
+     * The creation date of the survey.
+     * @type {Date}
+     */
     creationDate: Date;
 }
 
+/**
+ * Interface representing the structure of a survey to be created.
+ */
 interface CreateSurvey {
+    /**
+     * The title of the survey.
+     * @type {string}
+     */
     title: string;
+
+    /**
+     * The list of recipe options.
+     * @type {Recipe[]}
+     */
     options: Recipe[];
 }
 
-export type {Survey, CreateSurvey,UpdateSurvey};
-
+export type { Survey, CreateSurvey, UpdateSurvey };

6ddbb81e3a173c1ab0d3c981f776f351d68c596d
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-09-09 00:31:46 +0200

    [FRONTEND-30]: fix SurveyEditView and SurveyHub snapshot test

--- frontend-react/src/tests/views/SurveyEditView.test.tsx
+++ frontend-react/src/tests/views/SurveyEditView.test.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import {render} from "@testing-library/react";
 import SurveyEditView from "../../views/SurveyEditView";
+import {UserService} from "../../services/UserService";
+import {MemoryRouter} from "react-router-dom";
+
+jest.mock("../../services/UserService");
+
+const mockIsLoggedIn = UserService.isLoggedIn as jest.Mock;
 
 describe("SurveyEditView Component", () => {
 
-    it('matches snapshot for SurveyEditView component', () => {
-        const { asFragment } = render(<SurveyEditView />);
+    test('matches snapshot for SurveyEditView component if user is logged in', () => {
+        mockIsLoggedIn.mockResolvedValue(true);
+        const { asFragment } = render(
+            <MemoryRouter initialEntries={["/survey/edit/:id"]}>
+                <SurveyEditView />
+            </MemoryRouter>
+        );
+
         expect(asFragment()).toMatchSnapshot();
     });
+
+    test('matches snapshot for SurveyEditView component if user is not logged in', () => {
+        mockIsLoggedIn.mockResolvedValue(false);
+        const { asFragment } = render(
+            <MemoryRouter initialEntries={["/survey/edit/:id"]}>
+                <SurveyEditView />
+            </MemoryRouter>
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
 })
--- frontend-react/src/tests/views/SurveyHub.test.tsx
+++ frontend-react/src/tests/views/SurveyHub.test.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import {render} from "@testing-library/react";
 import SurveyHub from "../../views/SurveyHub";
+import {UserService} from "../../services/UserService";
+import {MemoryRouter} from "react-router-dom";
+
+jest.mock("../../services/UserService");
+
+const mockIsLoggedIn = UserService.isLoggedIn as jest.Mock;
 
 describe("SurveyHub Component", () => {
 
-    it('matches snapshot for SurveyHub component', () => {
-        const { asFragment } = render(<SurveyHub />);
+    test('matches snapshot for SurveyHub component if user is logged in', () => {
+        mockIsLoggedIn.mockResolvedValue(true);
+        const { asFragment } = render(
+            <MemoryRouter initialEntries={["/survey"]}>
+                <SurveyHub />
+            </MemoryRouter>
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test('matches snapshot for SurveyHub component if user is not logged in', () => {
+        mockIsLoggedIn.mockResolvedValue(false);
+        const { asFragment } = render(
+            <MemoryRouter initialEntries={["/survey"]}>
+                <SurveyHub />
+            </MemoryRouter>
+        );
+
         expect(asFragment()).toMatchSnapshot();
     });
 })

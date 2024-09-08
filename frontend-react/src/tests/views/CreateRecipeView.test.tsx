555c21fead925a7175fbb503924bc533b1e79282
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-09-08 16:39:52 +0200

    [FRONTEND-30]: add CreateRecipeView snapshot test

---
+++ frontend-react/src/tests/views/CreateRecipeView.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import CreateRecipeView from "../../views/CreateRecipeView";
+import { UserService } from "../../services/UserService";
+
+jest.mock("../../services/UserService");
+
+const mockIsLoggedIn = UserService.isLoggedIn as jest.Mock;
+
+describe("CreateRecipeView Component", () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it("redirects to login if user is not logged in", async () => {
+        mockIsLoggedIn.mockResolvedValue(false);
+        render(
+            <Router>
+                <CreateRecipeView recipe={null} />
+            </Router>
+        );
+        await waitFor(() => expect(window.location.pathname).toBe("/login"));
+    });
+
+    it("renders correctly when user is logged in and matches snapshot", async () => {
+        mockIsLoggedIn.mockResolvedValue(true);
+
+        const { asFragment } = render(
+            <Router>
+                <CreateRecipeView recipe={null} />
+            </Router>
+        );
+
+        await waitFor(() => {
+            expect(asFragment()).toMatchSnapshot();
+        });
+    });
+});

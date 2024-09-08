e73ce02db8bf833c3fd4b4375a6e31f13d010faf
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-09 00:09:10 +0200

    [ORGA-6]: service docs

--- frontend-react/src/services/ProductService.ts
+++ frontend-react/src/services/ProductService.ts
@@ -1,10 +1,22 @@
 import { Product } from "../types/Products";
-import {request} from "./Requests";
+import { request } from "./Requests";
 
+/**
+ * Service class for handling product-related operations.
+ */
 class ProductsService {
+    /**
+     * The backend URL for the product service.
+     * @type {string}
+     * @private
+     */
     private static backendURL: string =
         process.env.REACT_APP_BACKEND_URL || "http://localhost:13000";
 
+    /**
+     * Fetches all products from the backend.
+     * @returns {Promise<Product[]>} A promise that resolves to an array of products.
+     */
     public static getAll(): Promise<Product[]> {
         return new Promise((resolve, reject) => {
             request(`${this.backendURL}/products`).then((response: Response) => {
